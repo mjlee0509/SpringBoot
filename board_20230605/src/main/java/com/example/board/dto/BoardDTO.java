13589402eb5e9e37accea15e3d4a70ffbfbd8044
mjlee0509 <mjlee0509@gmail.com>
2023-06-05 16:28:57 +0900

    Day 72. 게시판: 글 조회 - 글 안열림 문제 해결 - jpa를 사용할 때 update/delete 쿼리를 수행하기 위해서는 @Transactional 어노테이션이 필요하다

--- board_20230605/src/main/java/com/example/board/dto/BoardDTO.java
+++ board_20230605/src/main/java/com/example/board/dto/BoardDTO.java
@@ -1,10 +1,7 @@
 package com.example.board.dto;
 
 import com.example.board.entity.BoardEntity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
--- board_20230605/src/main/java/com/example/board/repo/BoardRepository.java
+++ board_20230605/src/main/java/com/example/board/repo/BoardRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     // update board_table set board_hits = board_hits+1 where id=?; 를 만들어야 하는데...
@@ -14,8 +15,9 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
      * 3. JPQL(Java Persistance Query Language) :: 필요한 쿼리를 직접 만들어서 적용하고자 할 때
      */
 
+    @Transactional
     @Modifying
-    @Query(value = "update BoardEntity b set b.boardHits=b.boardHits+1 where b.id=:id")
+    @Query(value = "update BoardEntity b set b.boardHits = b.boardHits+1 where b.id=:id")
 //    @Query(value = "update board_table b set b.boardHits=b.boardHits+1 where b.id=:id", nativeQuery = true)
     /**
      * 주의사항 1. 접근하고자 하는 table이 아닌 Entity로 접근해줘야 한다
